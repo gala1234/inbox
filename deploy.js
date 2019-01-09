7c36580d2b8f5fb4d0130761919fb9261db90b0a
gala1234 <galadriel.calero@gig.com>
2019-01-09 15:55:18 +0100

    basic setup

--- deploy.js
+++ deploy.js
@@ -1,17 +1,21 @@
-pragma solidity ^0.4.25;
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
 
-contract Inbox {
-    string private message;
-    
-    function Inbox(string _message) public  {
-        message = _message;
-    }
-    
-    function setMessage (string _message) public {
-        message = _message;
-    }
-    
-    function getMessage() public view returns(string) {
-        return message;
-    }
-}
+const provider = new HDWalletProvider(
+    'call glow acoustic vintage front ring trade assist shuffle mimic volume reject',
+    'https://'
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+
+    console.log('Attempting to deply from account', account[0]);
+
+    const result = await web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ['Hi there'] })
+    .send({ gas: '1000000', from: accounts[0] });
+};
+
+deploy();
